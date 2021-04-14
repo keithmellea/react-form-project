205673140de530942c8945a206b63f75c9acc9a0
Keith Mellea <keithmellea@gmail.com>
2021-04-14 17:33:26 -0400

    building out form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { format } from 'date-fns';
+
+const uRF = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [staff, setStaff] = useState('');
+  const [bio, setBio] = useState('');
+  const [signUp, setSignUp] = useState('');
+
+  console.log(validationErrors);
+
+  
+
+
+
+}
 
 function App() {
   return (
